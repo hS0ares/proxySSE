7f338012dc02ec727d9529d015e49762d654de53
Haroldo Soares <haroldhrs@hotmail.com>
2024-11-07 00:44:20 -0300

    mock file server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
@@ -47,7 +48,7 @@ app.get('/sse-proxy', async (req, res) => {
       headers: {
         'Authorization': authToken,
         'X-Org-Id': orgId,
-        'Accept': '*/*'
+        //'Accept': ''
       }
     });
 
@@ -97,3 +98,32 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+*/
+
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Habilita CORS para todas as origens
+app.use(cors());
+
+// Configuração para SSE (Server-Sent Events)
+app.get('/sse', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Permite todas as origens
+
+    // Envia uma mensagem a cada 3 segundos
+    setInterval(() => {
+        res.write('data: { "message": "Ping" }\n\n');
+    }, 3000);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
+
+
